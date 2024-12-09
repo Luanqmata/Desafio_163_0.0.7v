920d104b88d93e1e158ec99dae009f29a8487dae
Shadow_CchiNnAa <138558778+Luanqmata@users.noreply.github.com>
2024-12-09 12:43:35 -0300

    Update main.go

--- desafio 163/main.go
+++ desafio 163/main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"carteira_163/encoding"
 	"fmt"
 	"math/rand"
 	"runtime"
 	"sync"
 	"time"
-	"carteira_163/encoding"
 )
 
 const (
 	caracters_btc = "0123456789abcdef"
-	endereco_163  = "1Hoyt6UBzwL5vvUSTLMQC2mwvvE5PpeSC" // CARTEIRA 163
+	carteira_163  = "1Hoyt6UBzwL5vvUSTLMQC2mwvvE5PpeSC" // CARTEIRA 163
 )
 
 var (
 	_wifindices_carteira = []string{"4", "3", "3", "4", "c", "f", "6", "9", "f", "3", "a", "c", "5", "0", "4", "b", "b", "7", "2", "6", "8", "7", "8", "a", "4", "0", "8", "3", "3", "3", "7", "3"}
 	memBuffer            = make([]byte, 4*1024*1024*1024)
 	ultima_Wif_gerada    string
+	// ultima_chave_gerada  string
 	mutex                sync.Mutex
+	encontrado           bool
+	wg                   sync.WaitGroup
 )
 
 func random_random() string {
@@ -27,38 +30,46 @@ func random_random() string {
 	return randomChar
 }
 
-func gerador_chave_163() {
+func gerador_wif_163() string {
 	var chaveBuilder string
 	for i := 0; i < 32 && i < len(_wifindices_carteira); i++ {
 		chaveBuilder += _wifindices_carteira[i]
 		randomChar := random_random()
 		chaveBuilder += randomChar
 	}
-
-	mutex.Lock()
-	ultima_Wif_gerada = chaveBuilder
-	mutex.Unlock()
+	return chaveBuilder
 }
 
-func worker() {
+func worker(id int) {
+	defer wg.Done()
+
 	for {
 		mutex.Lock()
-		chave := ultima_Wif_gerada
+		if encontrado {
+			mutex.Unlock()
+			return
+		}
 		mutex.Unlock()
 
-		hash_chave_pub := encoding.CreatePublicHash160(chave)
-		endereco := encoding.EncodeAddress(hash_chave_pub)
+		wif := gerador_wif_163()
+		pubKeyHash := encoding.CreatePublicHash160(wif)
+		carteira := encoding.EncodeAddress(pubKeyHash)
 
-		if endereco == endereco_163 {
-			fmt.Println("Endereço correto encontrado:", endereco)
-			break
+		mutex.Lock()
+		ultima_Wif_gerada = wif
+		if carteira == carteira_163 {
+			fmt.Print("Carteira Encontrada!!\n")
+			fmt.Print(carteira, "\n")
+			fmt.Print(wif, "\n")
+			encontrado = true
 		} else {
-			fmt.Println("Carteira:",endereco,"\n Wif: ",chave)
+			fmt.Print("\n carteira: ", carteira)
+			fmt.Print("\n wif: ", wif)
+		}
+		mutex.Unlock()
 
-			mutex.Lock()
-			ultima_Wif_gerada = ""
-			mutex.Unlock()
-			gerador_chave_163()
+		if encontrado {
+			return
 		}
 	}
 }
@@ -66,10 +77,11 @@ func worker() {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	runtime.GOMAXPROCS(8)
+	
+	for i := 0; i < 8; i++ {
+		wg.Add(1)
+		go worker(i)
+	}
 
-	go worker()
-
-	gerador_chave_163()
-
-	select {}
+	wg.Wait()
 }
