ce33589c2e400d6c239e555091dc31695c34f97e
Shadow_CchiNnAa <138558778+Luanqmata@users.noreply.github.com>
2024-12-14 22:33:17 -0300

    Update captura_comando.go

--- desafio 163/mecanicas/captura_comando.go
+++ desafio 163/mecanicas/captura_comando.go
@@ -7,14 +7,13 @@ import (
 	"syscall"
 )
 
-// CapturaComando captura sinais do sistema para encerrar o programa
 func CapturaSinal() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 
 	go func() {
 		<-signalChan
-		fmt.Println("\nO usuário escolheu sair...")
+		fmt.Println("\n\n\tInterrupção Teclado . . .\n\tPrograma Finalizado!")
 		os.Exit(0)
 	}()
 }
