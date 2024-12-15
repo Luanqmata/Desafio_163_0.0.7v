e2c4c6c523e596df78e8bc9c715416f43a26eb7e
Shadow_CchiNnAa <138558778+Luanqmata@users.noreply.github.com>
2024-12-14 22:34:37 -0300

    Update main.go

--- desafio 163/main.go
+++ desafio 163/main.go
@@ -1,33 +1,35 @@
 package main
 
 import (
+	"carteira_163/encoding"
 	"carteira_163/mecanicas"
+	"carteira_163/style"
 	"carteira_163/workers"
 	"fmt"
 	"runtime"
 	"sync"
+	"time"
 )
 
-const wif = "403d3x4xcxfx6x9xfx3xaxcx5x0x4xbxbx7x2x6x8x7x8xax4x0x8x3x3x3x7x3x"
-
 func main() {
-	fmt.Print("Bem vindo ao desafio 163 -- By China/gpt 0.2v\n\n")
+	style.Bem_vindo()
 
-	fmt.Print("1 ~~ 4\n2 ~~ 8\n3 ~~ 12\n4 ~~ 16 \n5 ~~ 20 \nDigite sua escolha: ")
+	encoding.InitWif("403b3d4xcxfx6x9xfx3xaxcx5x0x4xbxbx7x2x6x8x7x8xax4x0x8x3x3x3x7x3x")
+
+	style.Opcoes_uso_proc()
+
 	var escolhaModo int
 	fmt.Scanln(&escolhaModo)
 
 	var numThreads int
 	switch escolhaModo {
 	case 1:
-		numThreads = 4
+		numThreads = 5
 	case 2:
-		numThreads = 8
+		numThreads = 11
 	case 3:
-		numThreads = 12
+		numThreads = 17
 	case 4:
-		numThreads = 16
-	case 5:
 		numThreads = runtime.NumCPU()
 	default:
 		numThreads = 4
@@ -38,9 +40,30 @@ func main() {
 	mecanicas.CapturaSinal()
 
 	var wg sync.WaitGroup
+
+	go func() {
+		ticker := time.NewTicker(2 * time.Second)
+		defer ticker.Stop()
+
+		var ultimoTotal int64
+
+		for range ticker.C {
+			geradas := workers.GetChavesGeradas()
+			kps := geradas - ultimoTotal
+			ultimoTotal = geradas
+
+			fmt.Printf("\rChaves geradas: %d | KPS: %d", geradas, kps)
+
+			if workers.Encontrado {
+				break
+			}
+		}
+		fmt.Println("\nProcesso concluído!")
+	}()
+
 	for i := 0; i < numThreads; i++ {
 		wg.Add(1)
-		go workers.Worker(i, wif, &wg)
+		go workers.Worker(i, "", &wg)
 	}
 
 	wg.Wait()
